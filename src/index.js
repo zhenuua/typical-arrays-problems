ec4f056ad0f908d9879246ceeec799b42ea60d0f
zhenuua <zhenuua@gmail.com>
2021-03-14 20:07:31 +0300

    feat: do functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
-
 exports.min = function min (array) {
-  return 0;
+  if( array === undefined || array.length == 0) {
+    return 0;
+  } else { 
+  return Math.min.apply(null, array);
+}
 }
 
+
 exports.max = function max (array) {
-  return 0;
+  if( array === undefined || array.length == 0) {
+    return 0;
+  } else { 
+  return Math.max.apply(null, array);
+}
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if( array === undefined || array.length == 0) {
+    return 0;
+  } else { 
+  let sum = 0;
+  for (let i = 0; i < array.length; i++){
+    sum += array[i];
+  }
+  return sum / array.length;
+}
 }
